01d067abd0e950632fdb55fb72af8e563dfdcbca
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-07 03:36:53 -0300

    added ´Block´ class methods

--- include/snake.hpp
+++ include/snake.hpp
@@ -4,9 +4,18 @@
 #define SNAKE_HPP
 
 namespace snake {
-class Body {
+/**
+ * @class Block
+ * @brief A part of snake, a block of it
+ *
+ */
+class Block {
    private:
     point::Point position;
+
+    double color;
+
+   public:
 };
 }  // namespace snake
 
