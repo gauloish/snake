3b62d26f5832e74fe92c7cd1df61e3eec12c40a0
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-07 04:04:21 -0300

    added sizes to game window

--- source/settings.cpp
+++ source/settings.cpp
@@ -1,8 +1,11 @@
 #include "../include/settings.hpp"
 
 namespace windows {
-int width = 640;
-int height = 480;
+int unit = 25;
+int size = unit * unit;
+
+int width = size;
+int height = size;
 
 char title[6] = "Snake";
 }  // namespace windows
