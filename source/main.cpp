0c1025d3e6f8e7cda12d071616a66e75f3fc26c2
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-08 15:43:23 -0300

    ohh sheesh, so sexy

--- source/main.cpp
+++ source/main.cpp
@@ -1,30 +1,6 @@
-#include <GLFW/glfw3.h>
-
-#include <cstdlib>
-#include <iostream>
+#include "../include/functions.hpp"
 
 int main() {
-    if (not glfwInit()) {
-        std::cout << "an error has been ocurried!";
-
-        std::exit(EXIT_FAILURE);
-
-        return 0;
-    }
-
-    GLFWwindow *window;
-
-    window = glfwCreateWindow(640, 480, "Simple example", nullptr, nullptr);
-
-    if (not window) {
-        std::cout << "an error has been ocurried!";
-
-        glfwTerminate();
-
-        std::exit(EXIT_FAILURE);
-    }
-
-    std::cout << "ended!";
-
-    glfwTerminate();
+    // Run The Game
+    functions::run();
 }
