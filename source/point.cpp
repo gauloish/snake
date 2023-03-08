6a1ad3c8212133b147c02d658a6967973733b002
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-07 21:05:39 -0300

    using ´Point::set()´ in ´Point::Point()´ constructors and added limits to point positions

--- source/point.cpp
+++ source/point.cpp
@@ -1,13 +1,12 @@
+#include <algorithm>
+
 #include "../include/point.hpp"
 
 namespace point {
 /**
  * @brief Initialize a null point
  */
-Point::Point(void) {
-    this->x = 0;
-    this->y = 0;
-}
+Point::Point(void) { this->set(0, 0); }
 
 /**
  * @brief Initialize a point with passed coordinates
@@ -15,10 +14,7 @@ Point::Point(void) {
  * @param x Abscissa (Horizontal)
  * @param y Ordinate (Vertical)
  */
-Point::Point(double x, double y) {
-    this->x = x;
-    this->y = y;
-}
+Point::Point(double x, double y) { this->set(x, y); }
 
 /**
  * @brief Set point coordinates
@@ -27,8 +23,8 @@ Point::Point(double x, double y) {
  * @param y Ordinate (Vertical)
  */
 void Point::set(double x, double y) {
-    this->x = x;
-    this->y = y;
+    this->x = std::min(std::max(x, -1.0), 1.0);
+    this->y = std::min(std::max(y, -1.0), 1.0);
 }
 
 /**
