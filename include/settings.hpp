6610486b94a7e2ad6ffb47d85808484d4b8e9b40
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-08 15:58:30 -0300

    turn variables extern

--- include/settings.hpp
+++ include/settings.hpp
@@ -2,6 +2,14 @@
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
 
-namespace settings {}  // namespace settings
+namespace settings {
+extern const unsigned unit;
+extern const unsigned size;
+
+extern int width;
+extern int height;
+
+extern char title[6];
+}  // namespace settings
 
 #endif
