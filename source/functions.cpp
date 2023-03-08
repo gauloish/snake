242fc80c81c2dc4a3668a30ed70dd49e7f927af9
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-08 15:42:39 -0300

    a first code sample, just for test

---
+++ source/functions.cpp
@@ -0,0 +1,11 @@
+#include "../include/functions.hpp"
+#include "../include/point.hpp"
+#include "../include/settings.hpp"
+#include "../include/snake.hpp"
+
+namespace functions {
+void run(void) {
+    snake::Snake snake = snake::Snake(settings::size);
+    // nothing
+}
+}  // namespace functions
