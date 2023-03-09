d3f454819318a900c7954cc75351d308e655a021
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-08 22:21:42 -0300

    added update() and render() functions

--- source/functions.cpp
+++ source/functions.cpp
@@ -1,11 +1,58 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
 #include "../include/functions.hpp"
 #include "../include/point.hpp"
 #include "../include/settings.hpp"
 #include "../include/snake.hpp"
 
 namespace functions {
+void update(GLFWwindow *window, snake::Snake &snake) {
+    int state = glfwGetKey(window, GLFW_KEY_ENTER);
+
+    if (state == GLFW_PRESS) {
+        std::cout << "space!\n";
+    }
+}
+
+void render(GLFWwindow *window, snake::Snake &snake) {
+    // do something
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
 void run(void) {
-    snake::Snake snake = snake::Snake(settings::size);
-    // nothing
+    if (not glfwInit()) {
+        std::cerr << "Error: GLFW did not initialized!";
+
+        return;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(settings::width, settings::height,
+                                          settings::title, nullptr, nullptr);
+
+    if (not window) {
+        std::cerr << "Error: GLFW window did not created!";
+
+        glfwTerminate();
+
+        return;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (not glfwWindowShouldClose(window)) {
+        snake::Snake snake = snake::Snake(settings::size);
+
+        while (not glfwWindowShouldClose(window)) {
+            update(window, snake);
+            render(window, snake);
+        }
+    }
+
+    glfwTerminate();
 }
 }  // namespace functions
