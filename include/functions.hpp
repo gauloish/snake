05c2c16df035ff1ed333b4fcd3bc3cd6dd28f2d7
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-08 22:21:34 -0300

    added update() and render() functions

--- include/functions.hpp
+++ include/functions.hpp
@@ -1,7 +1,16 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// #include "../include/point.hpp"
+// #include "../include/settings.hpp"
+#include "../include/snake.hpp"
+
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 
 namespace functions {
+void update(GLFWwindow *, snake::Snake &);
+void render(GLFWwindow *, snake::Snake &);
 void run(void);
 }  // namespace functions
 
