e48148851287841e83d18278f0220a113cf547ca
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-13 14:52:00 -0300

    shitt

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -3,31 +3,6 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-namespace geometry {
-/**
- * @class Vertex
- * @brief Emulate a vertex with position and color
- *
- */
-struct Vertex {
-    double position[3];
-    double color[3];
-
-    Vertex(void);
-    Vertex(double[3], double[3]);
-};
-
-/**
- * @class Polygon
- * @brief A
- *
- */
-struct Polygon {
-    std::vector<Vertex> vertices;
-
-    Polygon(void);
-    Polygon(std::vector<Vertex>);
-};
-}  // namespace geometry
+namespace geometry {}  // namespace geometry
 
 #endif
