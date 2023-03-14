b41edc291fd883d2b54714da3cf3d7de6f7e9ccf
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-14 04:44:09 -0300

    added constructors and methods to set RGB colors components individually

--- source/geometry.cpp
+++ source/geometry.cpp
@@ -1,3 +1,7 @@
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+
 #include <algorithm>
 
 #include "../include/geometry.hpp"
@@ -16,9 +20,17 @@ Block::Block(void) { this->set(0.0, 0.0, 0.0, 0.0); }
  * @param step Size of block side
  * @param color Block color
  */
-Block::Block(double x, double y, double step, double color) {
-    this->set(x, y, step, color);
-}
+Block::Block(GLfloat x, GLfloat y, GLfloat step, GLfloat color) { this->set(x, y, step, color); }
+
+/**
+ * @brief Initialize a block with given values
+ *
+ * @param x Horizontal position
+ * @param y Vertical position
+ * @param step Size of block side
+ * @param color Block color array
+ */
+Block::Block(GLfloat x, GLfloat y, GLfloat step, GLfloat *color) { this->set(x, y, step, color); }
 
 /**
  * @brief Set a block with given values
@@ -28,10 +40,25 @@ Block::Block(double x, double y, double step, double color) {
  * @param step Size of block side
  * @param color Block color
  */
-void Block::set(double x, double y, double step, double color) {
-    this->vertices[0] = Vertex{x, y, 1.0, color, color, color};
-    this->vertices[1] = Vertex{x + step, y, 1.0, color, color, color};
-    this->vertices[2] = Vertex{x + step, y - step, 1.0, color, color, color};
-    this->vertices[3] = Vertex{x, y - step, 1.0, color, color, color};
+void Block::set(GLfloat x, GLfloat y, GLfloat step, GLfloat color) {
+    this->vertices[0] = Vertex{x, y, 0.0, color, color, color};
+    this->vertices[1] = Vertex{x + step, y, 0.0, color, color, color};
+    this->vertices[2] = Vertex{x + step, y - step, 0.0, color, color, color};
+    this->vertices[3] = Vertex{x, y - step, 0.0, color, color, color};
+}
+
+/**
+ * @brief Set a block with given values
+ *
+ * @param x Horizontal position
+ * @param y Vertical position
+ * @param step Size of block side
+ * @param color Block color array
+ */
+void Block::set(GLfloat x, GLfloat y, GLfloat step, GLfloat *color) {
+    this->vertices[0] = Vertex{x, y, 0.0, color[0], color[1], color[2]};
+    this->vertices[1] = Vertex{x + step, y, 0.0, color[0], color[1], color[2]};
+    this->vertices[2] = Vertex{x + step, y - step, 0.0, color[0], color[1], color[2]};
+    this->vertices[3] = Vertex{x, y - step, 0.0, color[0], color[1], color[2]};
 }
 }  // namespace geometry
