e6b31a34472a5b020604847f2f9fca48599ac479
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-14 05:10:11 -0300

    declared and implemented some functions

--- include/functions.hpp
+++ include/functions.hpp
@@ -3,16 +3,18 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-// #include "../include/point.hpp"
-// #include "../include/settings.hpp"
+#include "../include/render.hpp"
 #include "../include/snake.hpp"
 
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 
 namespace functions {
+int randomic(int, int);
+void init(snake::Snake &, render::Object &);
 void update(GLFWwindow *, snake::Snake &);
-void render(GLFWwindow *, snake::Snake &);
+void render(GLFWwindow *, snake::Snake &, render::Object &);
+void reshape(GLFWwindow *);
 void run(void);
 }  // namespace functions
 
--- source/functions.cpp
+++ source/functions.cpp
@@ -4,13 +4,35 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <random>
 
 #include "../include/functions.hpp"
 #include "../include/geometry.hpp"
+#include "../include/render.hpp"
 #include "../include/settings.hpp"
 #include "../include/snake.hpp"
 
 namespace functions {
+int randomic(int inferior, int superior) {
+    std::uniform_int_distribution<int> uniform(inferior, superior);
+    std::default_random_engine engine;
+
+    return uniform(engine);
+}
+
+void init(snake::Snake &snake, render::Object &base) {
+    std::random_device device;
+    std::mt19937 generator(device());
+
+    int color = randomic(0, 7);
+
+    glClearColor(settings::fore[color][0], settings::fore[color][1], settings::fore[color][2], 1.0);
+
+    snake.set(0.0, 0.0, 2.0 / (GLfloat)settings::unit, 0.9);
+    base.set(0.5, 0.5, 2.0 / (GLfloat)settings::unit, 0.5);
+    // base.set(-0.5, 0.5, 0.5, settings::back[color]);
+}
+
 /**
  * @brief Update the global states of objects and capture events
  *
@@ -18,23 +40,37 @@ namespace functions {
  * @param snake The snake object
  */
 void update(GLFWwindow *window, snake::Snake &snake) {
-    int keys[4][3] = {
+    GLint keys[4][3] = {
         {GLFW_KEY_LEFT, GLFW_KEY_H, GLFW_KEY_A},   // Left
         {GLFW_KEY_RIGHT, GLFW_KEY_L, GLFW_KEY_D},  // Right
         {GLFW_KEY_DOWN, GLFW_KEY_J, GLFW_KEY_S},   // Down
         {GLFW_KEY_UP, GLFW_KEY_K, GLFW_KEY_W},     // Right
     };
 
-    int senses[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    GLint senses[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     for (int sense = 0; sense < 4; sense++) {
         for (int key = 0; key < 3; key++) {
-            int state = glfwGetKey(window, keys[sense][key]);
+            GLint state = glfwGetKey(window, keys[sense][key]);
 
             if (state == GLFW_PRESS) {
                 snake.sense(senses[sense][0], senses[sense][1]);
             }
         }
+    }
+}
+
+void reshape(GLFWwindow *window) {
+    GLint width, height;
+
+    glfwGetFramebufferSize(window, &width, &height);
+
+    GLfloat position = (width - height) / 2.0;
+
+    if (height < width) {
+        glViewport(position, 0, height, height);
+    } else {
+        glViewport(0, -position, width, width);
     }
 }
 
@@ -44,8 +80,11 @@ void update(GLFWwindow *window, snake::Snake &snake) {
  * @param window Window where will be rendered stuffs
  * @param snake The snake object
  */
-void render(GLFWwindow *window, snake::Snake &snake) {
-    // do something
+void render(GLFWwindow *window, snake::Snake &snake, render::Object &base) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    base.draw();
+    snake.draw();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -61,8 +100,15 @@ void run(void) {
         return;
     }
 
-    GLFWwindow *window = glfwCreateWindow(settings::width, settings::height,
-                                          settings::title, nullptr, nullptr);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(settings::width, settings::height, settings::title, nullptr, nullptr);
 
     if (not window) {
         std::cerr << "Error: GLFW window did not created!";
@@ -77,11 +123,12 @@ void run(void) {
     GLenum status = glewInit();
 
 #ifdef GLEW_ERROR_NO_GLX_DISPLAY
-    if (status != GLEW_OK && status != GLEW_ERROR_NO_GLX_DISPLAY)
+    GLboolean error = (status != GLEW_OK && status != GLEW_ERROR_NO_GLX_DISPLAY);
 #else
-    if (status != GLEW_OK)
+    GLboolean error = (status != GLEW_OK);
 #endif
-    {
+
+    if (error) {
         std::cerr << "Error: " << glewGetErrorString(status) << "\n";
 
         glfwTerminate();
@@ -93,17 +140,16 @@ void run(void) {
     std::cout << "Status: Using GLEW   " << glewGetString(GLEW_VERSION) << "\n";
     std::cout << "Status: Using GLFW   " << glfwGetVersionString() << "\n";
 
-    GLuint buffer;
-    glGenBuffers(1, &buffer);
-
-    std::cout << "Buffer: " << buffer << "\n";
-
     while (not glfwWindowShouldClose(window)) {
         snake::Snake snake = snake::Snake(settings::size);
+        render::Object base = render::Object();
+
+        init(snake, base);
 
         while (not glfwWindowShouldClose(window)) {
+            reshape(window);
             update(window, snake);
-            render(window, snake);
+            render(window, snake, base);
         }
     }
 
